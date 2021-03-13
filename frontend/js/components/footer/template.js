9b834f52ff26cfe8873b1d75b15fc6d1931485d3
Karan Batta <krnbatta@gmail.com>
2021-03-13 06:02:04 +0530

    bold footer

--- frontend/js/components/footer/template.js
+++ frontend/js/components/footer/template.js
@@ -1,5 +1,5 @@
 let template = () => `
 <p class="footer-monash">Developed with love at Monash University <img width='40px' height='40px' src="images/monash.png"></p>
-<p class="footer-credits"><span>Supervisors: Dr Daniel Harabor, Dr Michael Wybrow</span> <span>Developed By Karan Batta - <a class="fab fa-linkedin" href="https://www.linkedin.com/in/krnbatta/" target="_blank"></a> <a class="fab fa-github-square" href="https://github.com/krnbatta/pathfinder" target="_blank"></a></span> </p>
+<p class="footer-credits"><span>Ideation & Management: <b>Dr Daniel Harabor</b>, <b>Dr Michael Wybrow</b></span> <span>Architecture & Development: <b>Karan Batta</b> - <a class="fab fa-linkedin" href="https://www.linkedin.com/in/krnbatta/" target="_blank"></a> <a class="fab fa-github-square" href="https://github.com/krnbatta/pathfinder" target="_blank"></a></span> </p>
 `;
 export default template;
