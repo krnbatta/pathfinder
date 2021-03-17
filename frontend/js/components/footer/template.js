c054917ed8d37fd1232ebda7a324f77edb01522a
Karan Batta <krnbatta@gmail.com>
2021-03-17 06:34:07 +0530

    added links in docs

--- frontend/js/components/footer/template.js
+++ frontend/js/components/footer/template.js
@@ -1,5 +1,5 @@
 let template = () => `
 <p class="footer-monash">Developed with love at Monash University <img width='40px' height='40px' src="images/monash.png"></p>
-<p class="footer-credits"><span>Ideation & Management: <b>Dr Daniel Harabor</b>, <b>Dr Michael Wybrow</b></span> <span>Architecture & Development: <b>Karan Batta</b> - <a class="fab fa-linkedin" href="https://www.linkedin.com/in/krnbatta/" target="_blank"></a> <a class="fab fa-github-square" href="https://github.com/krnbatta/pathfinder" target="_blank"></a></span> </p>
+<p class="footer-credits"><span>Ideation & Management: <b>Dr Daniel Harabor</b>, <b>Dr Michael Wybrow</b></span> <span>Architecture & Development: <b>Karan Batta</b> - <a href="https://www.linkedin.com/in/krnbatta/" target="_blank"><i class="fab fa-linkedin"></i></a> <a href="https://github.com/krnbatta/pathfinder" target="_blank"><i class="fab fa-github-square"></i></a></span> </p>
 `;
 export default template;
